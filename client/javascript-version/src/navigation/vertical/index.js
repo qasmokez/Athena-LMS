41f036d256980f2afaa793e45eabd229dd8ccc50
Zhicheng-wan <zhicheng3025938535@gmail.com>
2024-08-01 15:53:11 -0700

    imported frontend
    login page is under client/javascript-version/src/pages/pages/login

---
+++ client/javascript-version/src/navigation/vertical/index.js
@@ -0,0 +1,71 @@
+// ** Icon imports
+import Login from 'mdi-material-ui/Login'
+import Table from 'mdi-material-ui/Table'
+import CubeOutline from 'mdi-material-ui/CubeOutline'
+import HomeOutline from 'mdi-material-ui/HomeOutline'
+import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
+import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
+import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
+import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
+import BookOpenOutline from 'mdi-material-ui/BookOpenOutline'
+
+const navigation = () => {
+  return [
+    {
+      title: '主页/Dashboard',
+      icon: HomeOutline,
+      path: '/'
+    },
+    {
+      title: '设置/Settings',
+      icon: AccountCogOutline,
+      path: '/account-settings'
+    },
+    {
+      title: '我的课堂/Courses',
+      icon: BookOpenOutline,
+      path: '/courses'
+    },
+    {
+      sectionTitle: 'Pages'
+    },
+    {
+      title: 'Login',
+      icon: Login,
+      path: '/pages/login',
+      openInNewTab: true
+    },
+    {
+      title: 'Register',
+      icon: AccountPlusOutline,
+      path: '/pages/register',
+      openInNewTab: true
+    },
+    {
+      title: 'Error',
+      icon: AlertCircleOutline,
+      path: '/pages/error',
+      openInNewTab: true
+    },
+    {
+      sectionTitle: 'User Interface'
+    },
+    {
+      title: 'Cards',
+      icon: CreditCardOutline,
+      path: '/cards'
+    },
+    {
+      title: 'Tables',
+      icon: Table,
+      path: '/tables'
+    },
+    {
+      icon: CubeOutline,
+      title: 'Form Layouts',
+      path: '/form-layouts'
+    }
+  ]
+}
+
+export default navigation
