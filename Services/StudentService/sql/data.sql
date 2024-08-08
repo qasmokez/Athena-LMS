adae5afc4adff578a9531c0f19391dae896a8722
jason-auradine <jason@auradine.com>
2024-08-08 16:48:51 -0700

    moved parents info to a seperate table

--- Services/StudentService/sql/data.sql
+++ Services/StudentService/sql/data.sql
@@ -8,31 +8,10 @@ VALUES (
     jsonb_build_object(
         'grade', '10',
         'class', '1',
-        -- 'name', 'Jason Zhang', -- already in AccountService/sql
         'gender', 'male',
         'dob', '2003-01-01', -- date of birth
         'address', '123 Nosh Mabollocks Ave.',
-        'govtID', '110101200301018618',
-        'parents', jsonb_build_array(
-            jsonb_build_object(
-                'name', 'Bro Zhang',
-                'relation', 'father',
-                'phone number', '123-456-7890',
-                'govtID', '110101194501018618'
-            ),
-            jsonb_build_object(
-                'name', 'Girl Zhang',
-                'relation', 'mother',
-                'phone number', '098-765-4321',
-                'govtID', '1101011945091718618'
-            ),
-            jsonb_build_object(
-                'name', 'LeBron James',
-                'relation', 'other',
-                'phone number', '999-999-4321',
-                'govtID', '110101196801018618'
-            )
-        )
+        'govtID', '110101200301018618'
     )
 );
 
@@ -42,26 +21,10 @@ VALUES (
     jsonb_build_object(
         'grade', '12',
         'class', '3',
-        -- 'name', 'Tony Wang', -- already in AccountService/sql
         'gender', 'female',
         'dob', '2003-01-02',
         'address', 'no.1 Beijing Street',
-        'govtID', '110101200301028618',
-        'parents', jsonb_build_array(
-            jsonb_build_object(
-                'name', 'Dad Wang',
-                'relation', 'father',
-                'phone number', '123-456-6666',
-                'govtID', '110101194301018618'
-            ),
-            jsonb_build_object(
-                'name', 'Mom Wang',
-                'relation', 'mother',
-                'phone number', '098-666-4321',
-                'govtID', '110101194201018618'
-            )
-        )
-
+        'govtID', '110101200301028618'
     )
 );
 
@@ -85,5 +48,57 @@ VALUES (
         'honor', '2nd place',
         'givenby', 'dr. Johnson',
         'givenon', '2020-01-02'
+    )
+);
+
+DELETE FROM parents;
+INSERT INTO parents(studentid, data)
+VALUES (
+    '11111111-1111-1111-1111-111111111111',
+    jsonb_build_object(
+        'name', 'Bro Zhang',
+        'relation', 'father',
+        'phone', '123-456-7890',
+        'govtID', '110101194501018618'
+    )
+);
+INSERT INTO parents(studentid, data)
+VALUES (
+    '11111111-1111-1111-1111-111111111111',
+    jsonb_build_object(
+        'name', 'Girl Zhang',
+        'relation', 'mother',
+        'phone', '098-765-4321',
+        'govtID', '1101011945091718618'
+    )
+);
+INSERT INTO parents(studentid, data)
+VALUES (
+    '11111111-1111-1111-1111-111111111111',
+    jsonb_build_object(
+        'name', 'LeBron James',
+        'relation', 'other',
+        'phone', '999-999-4321',
+        'govtID', '110101196801018618'
+    )
+);
+INSERT INTO parents(studentid, data)
+VALUES (
+    '22222222-2222-2222-2222-222222222222',
+    jsonb_build_object(
+        'name', 'Dad Wang',
+        'relation', 'father',
+        'phone', '123-456-6666',
+        'govtID', '110101194301018618'
+    )
+);
+INSERT INTO parents(studentid, data)
+VALUES (
+    '22222222-2222-2222-2222-222222222222',
+    jsonb_build_object(
+        'name', 'Mom Wang',
+        'relation', 'mother',
+        'phone', '098-666-4321',
+        'govtID', '110101194201018618'
     )
 );
--- Services/StudentService/sql/schema.sql
+++ Services/StudentService/sql/schema.sql
@@ -10,6 +10,13 @@ CREATE TABLE student (
     data jsonb
 );
 
+DROP TABLE IF EXISTS parents;
+CREATE TABLE parents (
+    id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
+    studentid UUID, -- internal id
+    data jsonb
+);
+
 DROP TABLE IF EXISTS attendance;
 CREATE TABLE attendance (
     studentid UUID, -- internal id
@@ -22,15 +29,20 @@ CREATE TABLE honors (
     data jsonb
 );
 
--- Additional
+-- Security
 GRANT SELECT ON student TO admin;
 GRANT UPDATE ON student TO admin;
 GRANT SELECT ON honors TO admin;
 GRANT UPDATE ON honors TO admin;
+GRANT SELECT ON parents TO admin;
+GRANT UPDATE ON parents TO admin;
 GRANT SELECT ON student TO studentpool;
 GRANT INSERT ON student TO studentpool;
 GRANT UPDATE ON student TO studentpool;
 GRANT SELECT ON honors TO studentpool;
 GRANT INSERT ON honors TO studentpool;
 GRANT UPDATE ON honors TO studentpool;
+GRANT SELECT ON parents TO studentpool;
+GRANT INSERT ON parents TO studentpool;
+GRANT UPDATE ON parents TO studentpool;
 
