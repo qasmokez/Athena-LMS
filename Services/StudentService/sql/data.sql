7645d1942b0309f44402b77c8c8209798b0153ee
qasmokez <jz41039917@gmail.com>
2024-08-08 15:09:23 -0700

    added more fields in student data db

--- Services/StudentService/sql/data.sql
+++ Services/StudentService/sql/data.sql
@@ -8,8 +8,31 @@ VALUES (
     jsonb_build_object(
         'grade', '10',
         'class', '1',
-        'name', 'Jason Zhang',
-        'gender', 'male'
+        -- 'name', 'Jason Zhang', -- already in AccountService/sql
+        'gender', 'male',
+        'dob', '2003-01-01', -- date of birth
+        'address', '123 Nosh Mabollocks Ave.',
+        'govtID', '110101200301018618',
+        'parents', jsonb_build_array(
+            jsonb_build_object(
+                'name', 'Bro Zhang',
+                'relation', 'father',
+                'phone number', '123-456-7890',
+                'govtID', '110101194501018618'
+            ),
+            jsonb_build_object(
+                'name', 'Girl Zhang',
+                'relation', 'mother',
+                'phone number', '098-765-4321',
+                'govtID', '1101011945091718618'
+            ),
+            jsonb_build_object(
+                'name', 'LeBron James',
+                'relation', 'other',
+                'phone number', '999-999-4321',
+                'govtID', '110101196801018618'
+            )
+        )
     )
 );
 
@@ -19,8 +42,26 @@ VALUES (
     jsonb_build_object(
         'grade', '12',
         'class', '3',
-        'name', 'Tony Wang',
-        'gender', 'female'
+        -- 'name', 'Tony Wang', -- already in AccountService/sql
+        'gender', 'female',
+        'dob', '2003-01-02',
+        'address', 'no.1 Beijing Street',
+        'govtID', '110101200301028618',
+        'parents', jsonb_build_array(
+            jsonb_build_object(
+                'name', 'Dad Wang',
+                'relation', 'father',
+                'phone number', '123-456-6666',
+                'govtID', '110101194301018618'
+            ),
+            jsonb_build_object(
+                'name', 'Mom Wang',
+                'relation', 'mother',
+                'phone number', '098-666-4321',
+                'govtID', '110101194201018618'
+            )
+        )
+
     )
 );
 
